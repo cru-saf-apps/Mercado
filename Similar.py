17b6f09097326fdc4828b355ef0a8641ada349df
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-27 12:54:09 -0300

    Update Similar.py

--- Similar.py
+++ Similar.py
@@ -105,3 +105,126 @@ for var in vars:
 df_comp = df[lista_vars].copy()
 
 st.write(df_comp)
+
+lista_ranges = []
+
+for coluna in df_comp.columns[9:]:
+  if coluna in vars_abs:
+    top = df_comp[coluna].sum()
+  else:
+    top = df_comp[coluna].mean()
+    
+  lista_ranges.append(top)
+  
+def _invert(x, limits):
+    """inverts a value x on a scale from
+    limits[0] to limits[1]"""
+    return limits[1] - (x - limits[0])
+
+def _scale_data(data, ranges):
+    """scales data[1:] to ranges[0],
+    inverts if the scale is reversed"""
+    for d, (y1, y2) in zip(data[1:], ranges[1:]):
+        assert (y1 <= d <= y2) or (y2 <= d <= y1)
+    x1, x2 = ranges[0]
+    d = data[0]
+    if x1 > x2:
+        d = _invert(d, (x1, x2))
+        x1, x2 = x2, x1
+    sdata = [d]
+    for d, (y1, y2) in zip(data[1:], ranges[1:]):
+        if y1 > y2:
+            d = _invert(d, (y1, y2))
+            y1, y2 = y2, y1
+        sdata.append((d-y1) / (y2-y1) 
+                     * (x2 - x1) + x1)
+    return sdata
+
+class ComplexRadar():
+    def __init__(self, fig, variables, ranges,
+                 n_ordinate_levels=6):
+        angles = np.arange(0, 360, 360./len(variables))
+
+        axes = [fig.add_axes([0.1,0.1,0.9,0.9],polar=True,
+                label = "axes{}".format(i)) 
+                for i in range(len(variables))]
+        l, text = axes[0].set_thetagrids(angles, 
+                                         labels=variables)
+        [txt.set_rotation(angle-90) for txt, angle 
+             in zip(text, angles)]
+        for ax in axes[1:]:
+            ax.patch.set_visible(False)
+            ax.grid("off")
+            ax.xaxis.set_visible(False)
+        for i, ax in enumerate(axes):
+            grid = np.linspace(*ranges[i], 
+                               num=n_ordinate_levels)
+            gridlabel = ["{}".format(round(x,2)) 
+                         for x in grid]
+            if ranges[i][0] > ranges[i][1]:
+                grid = grid[::-1] # hack to invert grid
+                          # gridlabels aren't reversed
+            gridlabel[0] = "" # clean up origin
+            ax.set_rgrids(grid, labels=gridlabel,
+                         angle=angles[i])
+            #ax.spines["polar"].set_visible(False)
+            ax.set_ylim(*ranges[i])
+        # variables for plotting
+        self.angle = np.deg2rad(np.r_[angles, angles[0]])
+        self.ranges = ranges
+        self.ax = axes[0]
+    def plot(self, data, *args, **kw):
+        sdata = _scale_data(data, self.ranges)
+        self.ax.plot(self.angle, np.r_[sdata, sdata[0]], *args, **kw)
+    def fill(self, data, *args, **kw):
+        sdata = _scale_data(data, self.ranges)
+        self.ax.fill(self.angle, np.r_[sdata, sdata[0]], *args, **kw)
+        
+categorias = lista_vars[9:]
+
+try:
+  radar = ComplexRadar(fig,categorias,lista_ranges)
+  
+  nome = df_comp['Jogador'].tolist()[0]
+  if df_comp['Pé'].tolist()[0] == 'direito':
+    pe = 'Destro'
+  elif df_comp['Pé'].tolist()[0] == 'esquerdo':
+    pe = 'Canhoto'
+  elif df_comp['Pé'].tolist()[0] == 'ambos':
+    pe = 'Ambidestro'
+  else:
+    pe = 'Desconhecido'
+
+  altura = df_comp['Altura'].tolist()[0]
+
+  aux_df = df_comp.loc[:, df_comp.columns != 'Jogador']
+  aux_df = aux_df.loc[:, aux_df.columns != 'Equipe atual']
+  aux_df = aux_df.loc[:, aux_df.columns != 'Equipe no ano']
+  aux_df = aux_df.loc[:, aux_df.columns != 'Posição']
+  aux_df = aux_df.loc[:, aux_df.columns != 'Idade']
+  aux_df = aux_df.loc[:, aux_df.columns != 'ID']
+  aux_df = aux_df.loc[:, aux_df.columns != 'Liga']
+  aux_df = aux_df.loc[:, aux_df.columns != 'Pé']
+  aux_df = aux_df.loc[:, aux_df.columns != 'Altura']
+
+  aux_df = aux_df.reset_index(drop=True)
+
+  lista_valores = []
+
+  for coluna in aux_df.columns:
+    if coluna in vars_abs:
+      lista_valores.append(aux_df[coluna].sum())
+    else:
+      lista_valores.append(aux_df[coluna].mean())
+
+  legenda = nome + " (" + str(altura) +"cm; Pé: "+pe+")"
+  radar.plot(lista_valores,label=legenda)
+
+
+  fig.legend()
+ 
+  st.subheader("Radar de Desempenho\n"+nome_busca1 + " ("+str(anos1[0])+" a "+str(anos1[1]) + ")")
+  st.pyplot(fig)
+  
+except:
+  st.write("Por favor selecione ao menos 2 variáveis de comparação")
