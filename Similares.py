d312932d7e1aff1e1dde362d15d95421bb61e7fb
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-27 11:31:51 -0300

    Update Similares.py

--- Similares.py
+++ Similares.py
@@ -7,18 +7,17 @@ from matplotlib import pyplot as plt
 def baixar_base(lista_anos,lista_ligas):
   base = pd.DataFrame() 
 
-for ano in lista_anos:
-  for liga in lista_ligas:
-    for item in range(1,3):
-      arquivo = str(liga)+'-'+ano+'-'+str(item)+'.csv'
-      df = pd.read_csv(arquivo,sep=';',decimal=',')
-      df['Ano'] = int(ano)
-      df['Liga'] = liga
-      base = base.append(df).drop_duplicates().reset_index(drop=True)
+  for ano in lista_anos:
+    for liga in lista_ligas:
+      for item in range(1,3):
+        arquivo = str(liga)+'-'+ano+'-'+str(item)+'.csv'
+        df = pd.read_csv(arquivo,sep=';',decimal=',')
+        df['Ano'] = int(ano)
+        df['Liga'] = liga
+        base = base.append(df).drop_duplicates().reset_index(drop=True)
 
   base = base.rename(columns={"Equipa dentro de um período de tempo seleccionado":"Equipe no ano","Equipa":"Equipe atual"})
   base = base.reset_index(drop=True)
-  
   return base
 
 lista_anos = []
