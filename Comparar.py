fdbd25043b6bc4b73341cab608a5d8448f88a891
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-28 11:48:04 -0300

    Update Comparar.py

--- Comparar.py
+++ Comparar.py
@@ -158,7 +158,7 @@ if anos1[1] > anos2[1]:
 else:
   ano2_range = anos2[1]
 
-st.write(base)
+st.write(base[(base.Ano>=ano1_range)&(base1.Ano<=ano2_range)])
 
 for coluna in df_comp.columns[9:]:
   top = base[(base.Ano>=ano1_range)&(base1.Ano<=ano2_range)].nlargest(1,coluna)[coluna].tolist()[0]
