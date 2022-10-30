07c641b7418d2e02c7853facb8ac727d0acbd9fd
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-10-30 16:45:06 -0300

    Update Top-Comparar.py

--- Top-Comparar.py
+++ Top-Comparar.py
@@ -157,7 +157,7 @@ def gen_df_show_pronto(df_jogs, vars_select):
     df_show = df_show.assign(Ranking = range(1,len(df_show)+1))
 
 
-
+df_show = gen_df_show_pronto(df_jogs,vars_select)
 
 lista_show = ['Ranking','Nota','Jogador','Posição','Equipe atual','Minutos']
 lista_show.extend(vars_select)
